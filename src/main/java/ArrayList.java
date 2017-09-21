7ca951f130fa5e1f7464c0740ea379ab3b9f6f9c
Viktor <viktor.orkush@gmail.com>
2017-09-21 22:13:27 +0300

    Додав методи remove(T) toArray, get, set, iterator,  contains, indexOf, add(int, T)

--- src/main/java/ArrayList.java
+++ src/main/java/ArrayList.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class ArrayList<E> implements List<E> {
     private static final int DEFAULT_CAPACITY = 10;
+    private int GROW_CAPACITY = DEFAULT_CAPACITY;
     private int size;
     private E[] data;
 
@@ -28,33 +31,86 @@ public class ArrayList<E> implements List<E> {
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iter();
+    }
+
+    private class Iter implements Iterator<E> {
+        int cursor = 0;
+        int lastReturn = -1;
+
+        @Override
+        public boolean hasNext() {
+            return cursor != size;
+        }
+
+        @Override
+        public E next() {
+            if(cursor >= size){
+                throw new NoSuchElementException();
+            }
+            lastReturn = cursor;
+            cursor++;
+            return ArrayList.this.get(lastReturn);
+        }
+
+        @Override
+        public void remove() {
+            if(lastReturn<0) throw new IllegalStateException();
+            ArrayList.this.remove(lastReturn);
+            cursor = lastReturn;
+            lastReturn--;
+        }
     }
 
     @Override
     public <T extends E> boolean contains(T element) {
-        return false;
+        return indexOf(element) >= 0;
     }
 
     @Override
     public <T extends E> boolean remove(T element) {
+        if (element == null) {
+            for (int index = 0; index < size; index++) {
+                if (data[0] == null) {
+                    remove(index);
+                    return true;
+                }
+            }
+        } else {
+            for (int index = 0; index < size; index++) {
+                if (element.equals(data[0])) {
+                    remove(index);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public E[] toArray() {
-        return (E[]) new Object[]{};
+        return (E[]) Arrays.copyOf(data, size);
     }
 
     @Override
     public E get(int index) {
-        return null;
+        rangeCheck(index);
+        return data[index];
+    }
+
+    private void rangeCheck(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("get index =" + index + "but array size = " + size);
+        }
     }
 
     @Override
     public <T extends E> E set(int index, T element) {
-        return null;
+        rangeCheck(index);
+        E oldObject = data[index];
+        data[index] = element;
+        return oldObject;
     }
 
     @Override
@@ -69,12 +125,38 @@ public class ArrayList<E> implements List<E> {
 
     @Override
     public <T extends E> int indexOf(T element) {
-        return 0;
+        if (element == null) {
+            for (int index = 0; index < size; index++) {
+                if (data[index] == null) {
+                    return index;
+                }
+            }
+        } else {
+            for (int index = 0; index < size; index++) {
+                if (element.equals(data[index])) {
+                    return index;
+                }
+            }
+        }
+        return -1;
     }
 
     @Override
     public <T extends E> void add(int index, T element) {
+        rangeCheck(index);
+        capacityCheck();
+        System.arraycopy(data, index, data, index + 1, size - index);
+        data[index] = element;
+    }
 
+    private void capacityCheck() {
+        if (GROW_CAPACITY <= size + 1) {
+            GROW_CAPACITY = GROW_CAPACITY + (GROW_CAPACITY >> 1);
+            if (GROW_CAPACITY < 0) {
+                throw new OutOfMemoryError();
+            }
+            data = Arrays.copyOf(data, GROW_CAPACITY);
+        }
     }
 
     @Override
