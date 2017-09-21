a7d92256b4e5f019fad306923167bdb0fceaae42
Viktor <viktor.orkush@gmail.com>
2017-09-21 22:23:04 +0300

    Додав методи toArray, set,  contains, indexOf, add(int, T) Та короткий опис

--- src/main/java/ArrayList.java
+++ src/main/java/ArrayList.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 public class ArrayList<E> implements List<E> {
     private static final int DEFAULT_CAPACITY = 10;
@@ -31,35 +30,7 @@ public class ArrayList<E> implements List<E> {
 
     @Override
     public Iterator<E> iterator() {
-        return new Iter();
-    }
-
-    private class Iter implements Iterator<E> {
-        int cursor = 0;
-        int lastReturn = -1;
-
-        @Override
-        public boolean hasNext() {
-            return cursor != size;
-        }
-
-        @Override
-        public E next() {
-            if(cursor >= size){
-                throw new NoSuchElementException();
-            }
-            lastReturn = cursor;
-            cursor++;
-            return ArrayList.this.get(lastReturn);
-        }
-
-        @Override
-        public void remove() {
-            if(lastReturn<0) throw new IllegalStateException();
-            ArrayList.this.remove(lastReturn);
-            cursor = lastReturn;
-            lastReturn--;
-        }
+        return null;
     }
 
     @Override
@@ -69,24 +40,10 @@ public class ArrayList<E> implements List<E> {
 
     @Override
     public <T extends E> boolean remove(T element) {
-        if (element == null) {
-            for (int index = 0; index < size; index++) {
-                if (data[0] == null) {
-                    remove(index);
-                    return true;
-                }
-            }
-        } else {
-            for (int index = 0; index < size; index++) {
-                if (element.equals(data[0])) {
-                    remove(index);
-                    return true;
-                }
-            }
-        }
         return false;
     }
 
+    //переводим ArrayList в масив
     @Override
     @SuppressWarnings("unchecked")
     public E[] toArray() {
@@ -95,16 +52,17 @@ public class ArrayList<E> implements List<E> {
 
     @Override
     public E get(int index) {
-        rangeCheck(index);
-        return data[index];
+        return null;
     }
 
+    //Перевірка не вийшов індекс за межі массиву
     private void rangeCheck(int index) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("get index =" + index + "but array size = " + size);
         }
     }
 
+    //Заміняємо елемент в указаному місці
     @Override
     public <T extends E> E set(int index, T element) {
         rangeCheck(index);
@@ -123,6 +81,8 @@ public class ArrayList<E> implements List<E> {
         return null;
     }
 
+    //шукаємо елемнент в смасиві, якщо знайшли то виртаємо його ІНДЕКС
+    //якщо не знайшли то -1
     @Override
     public <T extends E> int indexOf(T element) {
         if (element == null) {
@@ -141,6 +101,7 @@ public class ArrayList<E> implements List<E> {
         return -1;
     }
 
+    //додаємо елемент у вказане місце, переміщачи всі елементи на один в право
     @Override
     public <T extends E> void add(int index, T element) {
         rangeCheck(index);
@@ -149,6 +110,7 @@ public class ArrayList<E> implements List<E> {
         data[index] = element;
     }
 
+    //перевіряємо вмістимість масива, якщо достигли максимуму збільшуємо в 2-ва ризи
     private void capacityCheck() {
         if (GROW_CAPACITY <= size + 1) {
             GROW_CAPACITY = GROW_CAPACITY + (GROW_CAPACITY >> 1);
