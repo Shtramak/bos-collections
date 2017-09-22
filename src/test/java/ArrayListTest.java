bc601927271c283220ce2dd039bfc35f9fdcc99d
Nebozavr <y.bartoshchuk@gmail.com>
2017-09-22 17:59:17 +0300

    Добавив тест до методу add

--- src/test/java/ArrayListTest.java
+++ src/test/java/ArrayListTest.java
@@ -1,2 +1,25 @@
+//Yura
+//        add
+//        remove(int)
+//        indexOf
+//        add(int, T)
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class ArrayListTest {
+
+    @Test
+    public void addTest(){
+        List<String> testList = new ArrayList<String>();
+        testList.add("test1");
+        testList.add("test2");
+        testList.add("test3");
+
+        String expected = "[test1, test2, test3]";
+
+        assertEquals(expected, testList.toString());
+    }
 }
