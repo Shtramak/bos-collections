c3c9e10697ec7ec93c7a869b5d3cd6ba307b506c
stramak <shtramak@gmail.com>
2017-09-30 11:42:07 +0300

    Merge LinkedList with Shtramak branch

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -1,5 +1,6 @@
 import java.util.Iterator;
 
+//shtramak: add, remove(int), indexOf, add(int, T), clear
 public class LinkedList<E> implements List<E> {
     private Node<E> first;
     private Node<E> last;
@@ -64,8 +65,6 @@ public class LinkedList<E> implements List<E> {
     }
 
 
-
-
     @Override
     public int size() {
         return 0;
@@ -88,22 +87,87 @@ public class LinkedList<E> implements List<E> {
 
     @Override
     public <T extends E> boolean add(T element) {
-        return false;
+        Node<E> l = last;
+        Node<E> newNode = new Node<>(l, element, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+        return true;
     }
 
     @Override
     public <T extends E> boolean remove(T element) {
+        if (element == null) {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (x.item == null) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (element.equals(x.item)) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public <T extends E> void add(int index, T element) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException("index " + index + " is out of bounds");
 
+        if (index == size) {
+            Node<E> l = last;
+            Node<E> newNode = new Node<>(l, element, null);
+            last = newNode;
+            if (l == null)
+                first = newNode;
+            else
+                l.next = newNode;
+            size++;
+        } else {
+            Node<E> pred = node(index).prev;
+            Node<E> newNode = new Node<>(pred, element, node(index));
+            node(index).prev = newNode;
+            if (pred == null)
+                first = newNode;
+            else
+                pred.next = newNode;
+            size++;
+        }
     }
+
+    void linkBefore(E e, Node<E> succ) {
+        // assert succ != null;
+        final Node<E> pred = succ.prev;
+        final Node<E> newNode = new Node<>(pred, e, succ);
+        succ.prev = newNode;
+        if (pred == null)
+            first = newNode;
+        else
+            pred.next = newNode;
+        size++;
+    }
+
 
     @Override
     public void clear() {
-
+        for (Node<E> x = first; x != null; ) {
+            Node<E> next = x.next;
+            x.item = null;
+            x.next = null;
+            x.prev = null;
+            x = next;
+        }
+        first = last = null;
+        size = 0;
     }
 
     @Override
@@ -123,11 +187,40 @@ public class LinkedList<E> implements List<E> {
 
     @Override
     public E remove(int index) {
-        return null;
+        if (index < 0 || index > size || size == 0)
+            throw new IndexOutOfBoundsException("index " + index + " is out of bounds");
+
+        return unlink(node(index));
     }
 
     @Override
     public <T extends E> int indexOf(T element) {
-        return 0;
+        int index = 0;
+        if (element == null) {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (x.item == null)
+                    return index;
+                index++;
+            }
+        } else {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (element.equals(x.item))
+                    return index;
+                index++;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) return "[]";
+        StringBuilder result = new StringBuilder("[");
+        for (Node<E> e = first; e != null; e = e.next) {
+            result.append(e.item).append(", ");
+        }
+        int startIndex = result.lastIndexOf(", ");
+        result.replace(startIndex, result.length(), "]");
+        return result.toString();
     }
 }
