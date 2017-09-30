8810334ab12f734032252e3e24a25d3e0d18267a
Nebozavr <y.bartoshchuk@gmail.com>
2017-09-30 13:22:20 +0300

    add test

--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -25,11 +25,50 @@ public class LinkedListTest {
     }
 
     @Test
+    public void testAddByIndexString() {
+        strings.add(0, "string0");
+        strings.add(2, "string2");
+        strings.add(5, "string5");
+
+        String expected = "[string0, string1, string2, string2, string3, string5]";
+        assertEquals(expected, strings.toString());
+    }
+
+    @Test
+    public void testAddByIndexInteger() {
+        integers.add(0, 100);
+        integers.add(7, 112);
+        integers.add(17, 115);
+
+        String expected = "[100, 0, 1, 2, 3, 4, 5, 112, 6, 7, 8, 9, 10, 11, 12, 13, 14, 115]";
+        assertEquals(expected, integers.toString());
+    }
+
+    @Test
+    public void testAddByIndexEmptyList() {
+        emptyList.add(0, 100);
+
+        String expected = "[100]";
+        assertEquals(expected, emptyList.toString());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddWithIndexOutOfBoundsException() {
+        try {
+            strings.add(-1, "Test22");
+            fail(); // if no exception was thrown
+        } catch (IndexOutOfBoundsException e) {
+            //Catch exception and go forward to check next step
+        }
+        strings.add(22, "Test22");
+    }
+
+    @Test
     public void testAddString() {
         strings.add("string4");
 
         String expected = "[string1, string2, string3, string4]";
-        assertEquals(expected,  Arrays.toString(strings.toArray()));
+        assertEquals(expected, strings.toString());
     }
 
     @Test
@@ -37,7 +76,7 @@ public class LinkedListTest {
         integers.add(100);
 
         String expected = "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 100]";
-        assertEquals(expected,  Arrays.toString(integers.toArray()));
+        assertEquals(expected, integers.toString());
     }
 
     @Test
@@ -46,7 +85,7 @@ public class LinkedListTest {
         emptyList.add("string5");
 
         String expected = "[1, string5]";
-        assertEquals(expected,  Arrays.toString(emptyList.toArray()));
+        assertEquals(expected, emptyList.toString());
     }
 
     @Test
@@ -63,6 +102,57 @@ public class LinkedListTest {
         assertFalse(expectedListStringRemove);
         assertFalse(expectedListStringRemoveNull);
         assertFalse(expectedEmpty);
+    }
+
+    @Test
+    public void testRemoveByIndex() {
+        String expectedString = strings.remove(1);
+        int expectedInt = integers.remove(2);
+
+        assertEquals("string2", expectedString);
+        assertEquals(2, expectedInt);
+        String expectedStrToString = "[string1, string3]";
+        assertEquals(expectedStrToString, strings.toString());
+
+        String expectedIntToString = "[0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]";
+        assertEquals(expectedIntToString, integers.toString());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveByIndexIndexOutOfBoundsException() {
+        try {
+            strings.remove(10);
+            fail();
+        } catch (IndexOutOfBoundsException e) {/*NOP*/}
+        try {
+            strings.remove(-1);
+            fail();
+        } catch (IndexOutOfBoundsException e) {/*NOP*/}
+        emptyList.remove(0);
+    }
+
+    @Test
+    public void testClear() {
+        strings.clear();
+        assertEquals("[]", strings.toString());
+        integers.clear();
+        assertEquals("[]", integers.toString());
+        emptyList.clear();
+        assertEquals("[]", emptyList.toString());
+    }
+
+    @Test
+    public void testIndexOf() {
+        int actual = strings.indexOf("string1");
+
+        assertEquals(0, actual);
+    }
+
+    @Test
+    public void testIndexOfNoElement() {
+        int actual = strings.indexOf("Error");
+
+        assertEquals(-1, actual);
     }
 
     @Test
